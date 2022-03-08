0cf1bd1a5819ab5cd63b043ae58f826252bdf7cc
jose pinto <jpor197402@gmail.com>
2022-03-08 04:39:57 +0000

    bug fixing

--- ghostproj/terraform/source/modulos/cdn/output.tf
+++ ghostproj/terraform/source/modulos/cdn/output.tf
@@ -3,3 +3,19 @@
   description = "aws_security_group__ecs_tasks_id"
   
 }  */
+
+
+  output "aws_cloudfront_distributions3_distribution_domain_name" {
+  value       = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+  description = "aws_security_group__ecs_tasks_id"
+  
+}  
+
+
+
+
+  output "aws_cloudfront_distributions3_distribution_hosted_zone_id" {
+  value       = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+  description = "aws_security_group__ecs_tasks_id"
+  
+}  
